50565db008bba5b245e5cc8bd4c1a6d5526b7447
dvsku <nikola.stojsin@hotmail.com>
2021-04-27 14:01:47 +0200

    Updated production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiURL: 'http://api.lvlupesports.pro:8443/',
-    imagesURL: 'http://api.lvlupesports.pro:8443/get-image/'
+    apiURL: 'https://api.lvlupesports.pro:8443/',
+    imagesURL: 'https://api.lvlupesports.pro:8443/get-image/'
 };
